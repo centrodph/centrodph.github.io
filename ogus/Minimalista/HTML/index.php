068b95149f1f0cb4de01ff668ebc6471d56e741f
GerardoMac <centrodph@gmail.com>
2017-05-24 07:32:32 -0300

    static ogus

---
+++ ogus/Minimalista/HTML/index.php
@@ -0,0 +1,260 @@
+<!DOCTYPE HTML>
+<!--[if lt IE 7]> <html class="no-js lt-ie10 lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie10 lt-ie9 lt-ie8 ie7" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie10 lt-ie9 ie8" lang="en"> <![endif]-->
+<!--[if IE 9 ]>   <html class="no-js lt-ie10 ie9" lang="en"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+<meta name="msvalidate.01" content="2D0C2D49381BF72093F8E664CFF3C49A"/>
+<link rel="shortcut icon" href="//static.mercadoshops.com/static/images/favicon.ico"/>
+<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0"/>
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<meta name="keywords" content="PRODUCTO 1, ogussh"/>
+<meta name="description" content="ogussh donde comprar Pintura"/>
+<title>ogussh</title>
+<link rel="stylesheet" type="text/css" media="screen" href="//static.mercadoshops.com/proxy/css?v=jk-master-b652-2017-05-22_15-28-00&env=prod&userVersion=V2&amp;f=themes/base.css,jquery-ui-1.7.2.custom.css,common/payment.css,themes/normalize.css,themes/chico-0.10.7.css,themes/mesh-0.9.3.css&amp;r=1495495324000&amp;for=252406637"/>
+<link rel="stylesheet" type="text/css" media="screen" href="//static.mercadoshops.com/proxy/css?v=jk-master-b652-2017-05-22_15-28-00&env=prod&userVersion=V2&amp;f=themes/mini/basic.css&amp;r=1495495324000&amp;for=252406637"/>
+<link rel="stylesheet" type="text/css" media="only screen and (min-width:640px) and (max-width:1024px)" href="//static.mercadoshops.com/proxy/css?v=jk-master-b652-2017-05-22_15-28-00&env=prod&userVersion=V2&amp;f=themes/mini/tablet.css&amp;r=1495495324000&amp;for=252406637"/>
+<link rel="stylesheet" type="text/css" media="only screen and (min-width:1024px)" href="//static.mercadoshops.com/proxy/css?v=jk-master-b652-2017-05-22_15-28-00&env=prod&userVersion=V2&amp;f=themes/mini/desktop.css&amp;r=1495495324000&amp;for=252406637"/>
+<!--[if lt IE 9 ]>
+			<link rel="stylesheet" type="text/css" media="screen" href="//static.mercadoshops.com/proxy/css?v=jk-master-b652-2017-05-22_15-28-00&env=prod&userVersion=V2&amp;f=themes/mini/desktop.css&amp;r=1495495324000&amp;for=252406637"/>
+			<![endif]-->
+<link rel="stylesheet" type="text/css" media="screen" href="//static.mercadoshops.com/proxy/css?v=jk-master-b652-2017-05-22_15-28-00&env=prod&userVersion=V2&amp;f=themes/mini/componentes.css&amp;r=1495495324000&amp;for=252406637"/>
+<link rel="stylesheet" type="text/css" media="screen" href="http://www.ogus.heisenbergstudio.com/styles/userlook.css?r=1495495324000"/>
+<link rel="stylesheet" type="text/css" media="screen" href="personalized.css"/>
+<script type="text/javascript" src="//static.mercadoshops.com/proxy/js?v=jk-master-b652-2017-05-22_15-28-00&amp;f=2012/modernizr-2.0.6.min.js&amp;"></script>
+<script type="text/javascript" src="//static.mercadoshops.com/proxy/js?v=jk-master-b652-2017-05-22_15-28-00&amp;f=analytics_final.js&amp;userVersion=V2"></script>
+</head>
+<body id="page-HOME">
+
+<div class="ogus_header">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-2">
+				<div class="ogus_header_logo">
+					<a href="/" title="Inicio">
+						<img src="https://centrodph.github.io/ogus/Minimalista/CSS/logo.png" alt="ogussh" title="ogussh"/>
+					</a>
+				</div>
+			</div>
+			<div class="col-sm-6">
+				<div class="ogus_header_menu">
+					<ul class="clearfix">
+						<li class="first background">
+						<a href="/" title="" class="link" data-name="Inicio" data-isFirstItem="true" data-target="/" data-noFollow="false" data-linkTarget="_self">Inicio</a>
+						</li>
+						<li class="background">
+						<a href="/shipping" title="" class="link" target="_self" data-name="Pago y Env&iacute;o" data-isFirstItem="false" data-target="/shipping" data-noFollow="false" data-linkTarget="_self">Pago y Env&iacute;o</a>
+						</li>
+						<li class="background">
+						<a href="/about" title="" class="link" target="_self" data-name="NUESTRA EMPRESA" data-isFirstItem="false" data-target="/about" data-noFollow="false" data-linkTarget="_self">NUESTRA EMPRESA</a>
+						</li>
+						<li class="background">
+						<a href="/contact" title="" class="link" target="_self" data-name="Contacto" data-isFirstItem="false" data-target="/contact" data-noFollow="false" data-linkTarget="_self">Contacto</a>
+						</li>
+					</ul>
+				</div>				
+			</div>
+			<div class="col-sm-4">
+				<div class="ogus_header_right">
+					<a href="javascript:void(0);">LOGIN</a>
+					<a href="javascript:void(0);">NEWSLETTER</a>
+					<a href="javascript:void(0);">0 CART</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+<header class="wrapper top">
+	<div class="ch-g1 header-top">
+	<div class="ch-container">
+	<figure class="component img-logo">
+	<a href="/" title="Inicio">
+	<img src="https://centrodph.github.io/ogus/Minimalista/CSS/logo.png" alt="ogussh" title="ogussh"/>
+	</a>
+	</figure>
+	<div id="searchWrap" class="searchWrapper clearfix">
+	<div class="component searchBar">
+	<form id="searchForm" method="get" action="/jm/com.mercadoshops.web.frontend.page.PageController">
+	<input type="hidden" name="view" value="result">
+	<input id="search" type="search" name="q" value="" class="inputField">
+	<input type="submit" value="Buscar" class="ch-btn ch-secondary button">
+	</form>
+	</div> <div id="cart" class="component cartButton">
+	<span id="mini-cart-container" class="bg-cart">
+	<a href="/cart" title="Ver Carrito" rel="nofollow" class="ch-btn ch-secondary button">Ver Carrito</a>
+	</span>
+	</div>
+	</div>
+	</div>
+	</div>
+	<div class="ch-g1 clearfix background border navWrapper">
+	<div class="ch-g1-5">
+	<div class="ch-container clearfix">
+	<a href="/pintura_qO26280686XtOcxSM" class="no-categ link ch-btn ch-secondary button">Todos los productos</a>
+	</div>
+	</div>
+	<div class="ch-g4-5">
+	<div class="ch-container clearfix">
+	<ul class="component simpleMenu clearfix">
+	<li class="first background">
+	<a href="/" title="" class="link" data-name="Inicio" data-isFirstItem="true" data-target="/" data-noFollow="false" data-linkTarget="_self">Inicio</a>
+	</li>
+	<li class="background">
+	<a href="/shipping" title="" class="link" target="_self" data-name="Pago y Env&iacute;o" data-isFirstItem="false" data-target="/shipping" data-noFollow="false" data-linkTarget="_self">Pago y Env&iacute;o</a>
+	</li>
+	<li class="background">
+	<a href="/about" title="" class="link" target="_self" data-name="NUESTRA EMPRESA" data-isFirstItem="false" data-target="/about" data-noFollow="false" data-linkTarget="_self">NUESTRA EMPRESA</a>
+	</li>
+	<li class="background">
+	<a href="/contact" title="" class="link" target="_self" data-name="Contacto" data-isFirstItem="false" data-target="/contact" data-noFollow="false" data-linkTarget="_self">Contacto</a>
+	</li>
+	</ul>
+	</div>
+	</div>
+	</div>
+</header>
+<div class="ch-g1 main wrapper" role="main">
+<div class="ch-g1 mainContent">
+<div class="ch-container">
+<div class="ch-container oneColumn clearfix">
+<div class="component promoMP payMethod clearfix">
+<span id="promo-block" class="promo-block"></span>
+<p class="pay-safe">Compra seguro:</p>
+<ul class="banks clearfix">
+<li><span class="hipotecario">Banco Hipotecario</span></li>
+<li><span class="icbc">Banco ICBC</span></li>
+<li><span class="macro">Banco Macro</span></li>
+<li><span class="tshopping">Banco Itaú</span></li>
+</ul>
+<p class="more">
+<a class="promosOFF" href="http://www.mercadopago.com/org-img/argentina/promociones-bancos.html" target="_blank" rel="nofollow">Más información</a>
+</p>
+</div>
+<section class="component prominentProd prodCarousel ch-g1">
+<div class="ch-carousel" data-max-items="1">
+<ul>
+<li>
+<article class="clearfix">
+<h2><a title="PRODUCTO 1" href="/producto--400156531xJM" class="title">PRODUCTO 1</a></h2>
+<figure>
+<a href="/producto--400156531xJM" title="PRODUCTO 1" class="link">
+<img src="http://mla-s2-p.mlstatic.com/919520-MLA25559886969_052017-C.jpg" alt="PRODUCTO 1" title="PRODUCTO 1">
+</a>
+</figure>
+<dl id="homePrice">
+<dd>
+<span class="ch-price price">$ 120<span class="decimal">,</span><sup>00</sup></span>
+</dd>
+</dl>
+<div class="itemFeatures"></div>
+</article>
+</li>
+</ul>
+</div>
+</section><div class="component newsProduct clearfix">
+<h3 class="title-component">Novedades</h3>
+<div class="ch-carousel" data-max-items="2">
+<ul class="list-prod">
+<li>
+<figure>
+<a href="/producto--400156531xJM">
+<img src="http://mla-s2-p.mlstatic.com/919520-MLA25559886969_052017-P.jpg" title="PRODUCTO 1"/>
+</a>
+</figure>
+<h2>
+<a href="/producto--400156531xJM" title="PRODUCTO 1" class="title">PRODUCTO 1</a>
+</h2>
+<dl id="homePrice">
+<dd>
+<span class="ch-price price">$ 120<span class="decimal">,</span><sup>00</sup></span>
+</dd>
+</dl>
+</li>
+</ul>
+</div>
+</div>
+<nav class="component itemGallery clearfix">
+<h3 class="title-component"></h3>
+<ul class="item-gallery">
+<li class="background ">
+<article class="clearfix ">
+<h2>
+<a href="/producto--400156531xJM" title="PRODUCTO 1" class="title">PRODUCTO 1</a>
+</h2>
+<ul class="itemData clearfix">
+<li>
+<figure class="background">
+<a class="link" title="PRODUCTO 1" href="/producto--400156531xJM">
+<img class="" alt="PRODUCTO 1" title="PRODUCTO 1" src="http://mla-s2-p.mlstatic.com/919520-MLA25559886969_052017-N.jpg">
+</a>
+</figure>
+</li>
+<dl id="homePrice">
+<dd>
+<span class="ch-price price">$ 120<span class="decimal">,</span><sup>00</sup></span>
+</dd>
+</dl>
+</ul>
+</article>
+</li>
+</ul>
+</nav>
+</div>
+</div>
+</div>
+</div>
+<footer class="ch-g1 wrapper bottom clearfix">
+<div class="ch-container">
+<address class="component addressExtended link">
+<dl class="companyData">
+<dt>Nuestros datos</dt>
+<dd>ogussh</dd>
+<dd>893 4291 San Francisco Solano, Quilmes, Buenos Aires</dd>
+<dd>11 20781160</dd>
+<dd>
+<a href="/contact">Contáctanos</a>
+</dd>
+</dl>
+<dl class="socialData">
+<dt>
+Seguinos:
+</dt>
+<dd id="fb-button">
+</dd>
+<g:plusone size="medium" count="false" href="http://www.ogus.heisenbergstudio.com"></g:plusone>
+</dl>
+</address>
+</div>
+<div class="component mshops">Powered by <a class="link" href="http://www.mercadoshops.com.ar" title="MercadoShops">MercadoShops</a></div>
+</footer>
+<div class="installments-info">Todos los precios están calculados en 1 pago.</div>
+<script src="static/js/analytics_utils.js"></script>
+<script type="text/javascript">
+						try {
+				/* global ga*/
+				// Creates the tracker
+				ga('create', 'UA-20537619-2', 'auto', trackers.mshopsTracker);
+				var domainExtension = ('CL' === 'AR') ? 'AR' : 'com.' + 'AR';
+				// Sets de dimensions
+				ga(trackers.mshopsTracker + '.set', 'dimension' + googleAnalytics.dimensions['CountryId'], 'AR');
+				ga(trackers.mshopsTracker + '.set', 'dimension' + googleAnalytics.dimensions['ShopId'], '252406637');
+				ga(trackers.mshopsTracker + '.set', 'dimension' + googleAnalytics.dimensions['Plan'], 'MS5');
+				ga(trackers.mshopsTracker + '.set', 'dimension' + googleAnalytics.dimensions['UseNewThemes'], 'false');
+				// Sends the pageview
+				var urlToPush = "";
+									urlToPush = '/AR/ogussh.mercadoshops.com.ar' + '' + document.location.pathname + document.location.search + document.location.hash
+				var pageViewParameters = getGaParameters();
+				pageViewParameters.page = urlToPush;
+				ga(trackers.mshopsTracker + '.set', pageViewParameters);
+				ga(trackers.mshopsTracker + '.send', 'pageview');
+			} catch (err) {
+			}
+				</script>
+        <script id="__bs_script__">//<![CDATA[
+            document.write("<script async src='http://HOST:8080/browser-sync/browser-sync-client.js?v=2.17.3'><\/script>".replace("HOST", location.hostname));
+        //]]></script>
+</body>
+</html>
